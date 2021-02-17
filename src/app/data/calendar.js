2489a466c77a55c34987f8adcdc6f83dc4d82d3f
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-17 16:55:35 -0300

    create data

--- src/app/data/calendar.js
+++ src/app/data/calendar.js
@@ -1,4 +1,4 @@
-export const data = [
+export const data_ = [
   '2021-02-05',
   '2021-02-07',
   '2021-02-10',
@@ -12,16 +12,278 @@ export const data = [
   '2021-02-30'
 ]
 
-export const data2 = [
-  '2021-02-05',
-  '2021-02-07',
-  '2021-02-10',
-  '2021-02-11',
-  '2021-02-12',
-  '2021-02-13',
-  '2021-02-17',
-  '2021-02-20',
-  '2021-02-21',
-  '2021-02-25',
-  '2021-02-30'
+export const data = [
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2020-07-01',
+    value: 1182.68,
+    paymentDate: '2020-12-09'
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/4',
+    date: '2020-07-01',
+    value: 1099.05,
+    paymentDate: '2020-07-01'
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2020-08-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/4',
+    date: '2020-08-01',
+    value: 1099.05,
+    paymentDate: '2020-08-01'
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2020-09-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/4',
+    date: '2020-09-01',
+    value: 1099.05,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/4',
+    date: '2020-10-01',
+    value: 1099.05,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2020-10-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/4',
+    date: '2020-11-01',
+    value: 1099.05,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2020-11-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2020-12-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2020-12-23',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2021-01-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-01-23',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2021-02-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-02-17',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-02-20',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Pafúncio ConstÇancio',
+    url: '/movement/21',
+    date: '2021-02-17',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Camil Alimentos SA',
+    url: '/movement/21',
+    date: '2021-02-17',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-02-14',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-02-16',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-02-23',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2021-03-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-03-15',
+    value: 568.88,
+    paymentDate: '2021-02-15'
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-03-23',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/5',
+    date: '2021-04-01',
+    value: 1182.68,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-04-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-04-23',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-05-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Studio Camila Katia',
+    url: '/movement/21',
+    date: '2021-05-23',
+    value: 1190.03,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-06-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-07-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-08-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-09-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-10-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-11-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2021-12-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2022-01-15',
+    value: 568.88,
+    paymentDate: null
+  },
+  {
+    title: 'Solicitante Palho\u00e7a - SC',
+    url: '/movement/25',
+    date: '2022-02-15',
+    value: 568.88,
+    paymentDate: null
+  }
 ]
